a3eeacbc26ddea07d66b98db0231214993b0f968
Nouno <arnauld841@gmail.com>
2022-05-23 15:42:12 +0200

    Test unitaire !

--- src/test/BusinessTaskModelTest.java
+++ src/test/BusinessTaskModelTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BusinessTaskModelTest {
-    //  Ici test unitaire peu utile ici car pas de calcul dans notre tâche métier suite au conseils du pro
+    //  Ici test unitaire peu utile ici car pas de calcul dans notre tâche métier suite aux conseils du proffesseur
     //  mais pour prouver que on sait les faire
     private BusinessTaskModel businessTaskModel;
 
